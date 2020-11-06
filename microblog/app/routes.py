7eb8e6657d825d516cee03c25059852c62a770bb
pavelok2015 <pavel.brodnizkiy@mail.ru>
2020-11-06 12:46:43 +0300

    Flask path 2

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Привет,мир!"
+    user = {'username': 'Pavel'}
+    posts = [
+        {
+            'author': {'username':'Pasha'},
+            'body': 'Beautiful day in trash!'
+        },
+        {
+            'author': {'username':'Матвей'},
+            'body': 'Щас бы котлетку с пюрешкой.'
+        },
+        {
+            'author': {'username':'Илья'},
+            'body': 'Что делать-то?'
+        }
+    ]
+    return render_template('index.html',title ='home', user = user,posts = posts)
