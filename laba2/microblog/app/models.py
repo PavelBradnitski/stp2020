88e88a19aa4c03452c6099ebd912949fbd2ad68b
pavelok2015 <pavel.brodnizkiy@mail.ru>
2020-11-20 19:34:35 +0300

    6 part Flask

--- laba2/microblog/app/models.py
+++ laba2/microblog/app/models.py
@@ -1,7 +1,10 @@
+from hashlib import md5
 from datetime import datetime
 from app import db,login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
+
 
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +12,8 @@ class User(UserMixin,db.Model):
     e_mail = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -17,7 +22,12 @@ class User(UserMixin,db.Model):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)   
+        return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.e_mail.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)       
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,6 +37,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
 
 @login.user_loader
 def load_user(id):
